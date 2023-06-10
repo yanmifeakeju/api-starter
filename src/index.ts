06dc130469f63c3ba02aab693b9f81d474954eed
yanmifeakeju <yanmifeakeju@gmail.com>
2023-06-10 02:50:38 +0100

    customer validation error formatter

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,27 @@ import Fastify, { FastifyReply, FastifyRequest } from 'fastify';
 import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 import { userRoutes } from './modules/users/users.routes.js';
 import { createRequire } from 'module';
-// import { schemaErrorMessageGenerator } from './utils/schema/errorMessage.js';
-import fastifyJwt from '@fastify/jwt';
 import { env } from './config/env.js';
+import { schemaErrorMessageGenerator } from './utils/formatter/errorFormatter.js';
 
 const require = createRequire(import.meta.url);
 
 const server = Fastify().withTypeProvider<TypeBoxTypeProvider>();
 
-// server.setErrorHandler((error, request, reply) => {
-//   if (error.validation) {
-//     reply.status(400).send({
-//       success: false,
-//       error: schemaErrorMessageGenerator(error.validation)
-//     });
+server.setErrorHandler((error, request, reply) => {
+  if (error.validation) {
+    reply.status(400).send({
+      success: false,
+      error: schemaErrorMessageGenerator(error.validation)
+    });
 
-//     return;
-//   }
+    return;
+  }
 
-//   reply
-//     .status(error.statusCode || 500)
-//     .send({ success: false, error: error.message });
-// });
+  reply
+    .status(error.statusCode || 500)
+    .send({ success: false, error: error.message });
+});
 
 server.get('/health-check', async () => {
   return { status: 'OK' };
